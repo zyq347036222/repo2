ee87401f2ae56ecfbb893cad88c123c1cf99256f
zyq347036222 <44916072+zyq347036222@users.noreply.github.com>
2019-02-27 13:31:43 +0800

    Update Main.java
    第二次修改卖你方法

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,13 +1,11 @@
 
 
 public class Main {
-
-
     public static void main(String[] args) {
-        System.out.println("hello,git");
-        System.out.println("hello,git");
-        System.out.println("hello,git");
-        System.out.println("hello,git");
-        System.out.println("hello,git");
+        System.out.println("hello,git1");
+        System.out.println("hello,git2");
+        System.out.println("hello,git3");
+        System.out.println("hello,git4");
+        System.out.println("hello,git5");
     }
 }
