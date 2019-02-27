c9feea5d69554ddd1affd2577715f4881a77cb75
zyq347036222 <44916072+zyq347036222@users.noreply.github.com>
2019-02-27 13:34:41 +0800

    Update Main.java
    110

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -2,7 +2,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello,git1");
+        System.out.println("hello,git10");
         System.out.println("hello,git2");
         System.out.println("hello,git3");
         System.out.println("hello,git4");
