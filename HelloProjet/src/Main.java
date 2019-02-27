6766e1416d4f0b00a19ad981ce7ee8a22875c6ba
yongqiang <123>
2019-02-27 13:25:53 +0800

    修改main方法

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,9 +1,13 @@
-import org.junit.Test;
+
 
 public class Main {
 
-    @Test
-    public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
+        System.out.println("hello,git");
     }
 }
